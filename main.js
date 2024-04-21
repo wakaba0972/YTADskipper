59db7221e21a66770d64e0400ecdbdf805de5f5f
wakaba0972 <benny.wu2199@gmail.com>
2024-04-21 16:35:37 +0800

    改用listener

--- main.js
+++ main.js
@@ -3,48 +3,25 @@
 // @namespace    http://tampermonkey.net/
 // @version      2024-04-09
 // @description  try to take over the world!
-// @author       WaKaBaP
+// @author       You
 // @match        https://www.youtube.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // ==/UserScript==
 
-const ADKEY = "ytp-ad-player-overlay";
+const ADKEY = "ytp-ad-text";
 const SKIPKEY = "ytp-ad-skip-button-modern ytp-button";
 
 function skipAD(){
-    console.log("Enter skipAD");
+    if(document.getElementsByClassName(ADKEY).length) {
+         //document.querySelector("video").playbackRate = 16;
+        document.querySelector("video").currentTime = 999;
+        if(document.getElementsByClassName(SKIPKEY).length) document.getElementsByClassName(SKIPKEY)[0].click();
 
-    if(!document.getElementsByClassName(ADKEY).length) {
-        console.log("Leave skipAD");
-        return;
+        console.log("AD SKIP");
     }
-
-    //document.querySelector("video").playbackRate = 16;
-    document.querySelector("video").currentTime = 999;
-
-    setTimeout(()=>{
-        document.getElementsByClassName(SKIPKEY)[0].click();
-    }, 500)
-
-    console.log("AD SKIP");
-
-    setTimeout(()=>{
-        skipAD();
-    }, 1000);
 }
 
 (function() {
     'use strict';
-
-    window.onload = function(){
-        setTimeout(()=>{
-            skipAD();
-        }, 1000);
-    }
-
-    window.navigation.addEventListener("navigate", (event) => {
-        setTimeout(()=>{
-            skipAD();
-        }, 1500);
-    });
+    setInterval(skipAD, 1000);
 })();
